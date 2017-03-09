13f8a7e54994c3037bf4cd02ec8080e4d79c3892
Jim X. Lin <miljinx@gmail.com>
2017-03-08 19:36:02 -0800

    Add missing changes for last commit

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,7 @@ class Game < ApplicationRecord
   has_many :participations
 
   scope :available,
-    # -> { where('(white_player_id = ? AND black_player_id != ?)
-    #             OR
-    #             (white_player_id != ? AND black_player_id = ?)',
-    #             nil, nil, nil, nil) }
-    -> { where('(white_player_id = ? OR black_player_id = ?)', nil, nil) }
+    -> { where('(white_player_id IS NULL AND black_player_id IS NOT NULL)
+                OR
+                (white_player_id IS NOT NULL AND black_player_id IS NULL)') }
 end
