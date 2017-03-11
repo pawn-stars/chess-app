70bb703cbfa0c256ece405d6835b5441ffef9b7f
Jim X. Lin <miljinx@gmail.com>
2017-03-10 21:52:31 -0800

    Fix lambda syntax

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,9 +3,9 @@ class Game < ApplicationRecord
   has_many :pieces
 
   scope :available,
-        lambda do
+        lambda {
           where('(white_player_id IS NULL AND black_player_id IS NOT NULL)
           OR
           (white_player_id IS NOT NULL AND black_player_id IS NULL)')
-        end
+        }
 end
