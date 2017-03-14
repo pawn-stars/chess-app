04bdc299d157fefd2c72b48a2537605d8e2251ce
Melissa Reynolds <mkreynolds@cableone.net>
2017-03-14 04:21:36 +0000

    Hompage list of games with button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,20 +1,45 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    @games = Game.available
   end
 
   def create
-    @game = Game.new(game_params)
+    # For Development Purposes Only - Used to test Available list.
+    Game.create(white_player_id: 1)
+
+    redirect_to games_path
+
+  end
+
+  def new
+    @game = Game.new
   end
 
   def show
   end
 
+# Join a Game
+# Current_user_id becomes black_player_id
+# For given game assign (update) current_user_id to black_player_id
+
+  def update
+    @game = Game.find(params[:id])
+
+    @game.update_attributes(game_params)
+    if @game.valid?
+      redirect_to games_path
+    else
+      render :index, text: "Not Allowed"
+    end
+  end
+  
+
   private
 
   def game_params
-    params.require(:games).permit(:white_player_id, :black_player_id)
+    params.require(:game).permit(:black_player_id, :white_player_id)
   end
+
 
 end
