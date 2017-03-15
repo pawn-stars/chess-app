31d76a19956e232995b8305565d07a16643c754f
Jim X. Lin <miljinx@gmail.com>
2017-03-14 21:33:25 -0700

    add Game::Board class

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,4 +8,30 @@ class Game < ApplicationRecord
           OR
           (white_player_id IS NOT NULL AND black_player_id IS NULL)')
         }
+
+  class Board
+    attr_reader :grid
+    
+    def initialize(pieces)
+      @pieces = pieces
+      @grid = Array.new(8) { Array.new(8) }
+      fill_grid
+    end
+    
+    private
+    
+    def pieces
+      @pieces
+    end
+    
+    def fill_grid
+      pieces.each do |piece|
+        next if piece.is_captured
+        if grid[piece.row][piece.col]
+          raise "uncaptured pieces occupy same square"
+        end
+        grid[piece.row][piece.col] = piece
+      end
+    end
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -23,4 +23,49 @@ RSpec.describe Game, type: :model do
       expect(Game.available.count).to eq(0)
     end
   end
+  
+  describe "Board" do
+    before(:all) do
+      if user = User.find_by(email: 'foobar@foobar.com')
+        @user = user
+      else
+        @user = User.create(
+          email: 'foobar@foobar.com',
+          screen_name: 'foobar',
+          password: 'foobar',
+          password_confirmation: 'foobar'
+        )
+      end
+      
+      @game = @user.games.create(white_player_id: @user);
+    end
+    
+    describe "#grid" do
+      it "returns an 8x8 array that may have Piece instances as elements" do
+        piece1 = @game.pieces.create(
+          row: 0, col: 0, is_captured: false, user: @user
+        )
+        piece2 = @game.pieces.create(
+          row: 7, col: 7, is_captured: false, user: @user
+        )
+        board = Game::Board.new(@game.pieces)
+
+        expect(board.grid[0][0]).to be(piece1)
+        expect(board.grid[7][7]).to be(piece2)
+      end
+    end
+    
+    describe "#fill_grid" do
+      it "raises an error if uncaptured pieces occupy the same square" do
+        piece1 = @game.pieces.create(
+          row: 0, col: 0, is_captured: false, user: @user
+        )
+        piece2 = @game.pieces.create(
+          row: 0, col: 0, is_captured: false, user: @user
+        )
+        
+        expect { Game::Board.new(@game.pieces) }.to raise_error(RuntimeError)
+      end
+    end
+  end
 end
