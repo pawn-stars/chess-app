a729467f4d92c9197bcf600815d2111bfc4495ba
Jim X. Lin <miljinx@gmail.com>
2017-03-14 18:06:01 -0700

    Rename pieces column from captured to is_captured

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314231937) do
+ActiveRecord::Schema.define(version: 20170315010400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 20170314231937) do
     t.string   "type"
     t.integer  "row"
     t.integer  "col"
-    t.boolean  "captured"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "is_captured"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "game_id"
     t.boolean  "is_black"
     t.integer  "user_id"
