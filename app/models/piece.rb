6066f8152301c91d5a985f5af5f32e465890dee0
Jim X. Lin <miljinx@gmail.com>
2017-03-15 20:57:22 -0700

    Add Piece#move_to!

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,4 +5,9 @@ class Piece < ApplicationRecord
   
   scope :are_captured, -> { where(is_captured: true) }
   scope :are_not_captured, -> { where(is_captured: false) }
+  
+  def move_to!(row, col)
+    raise "Out of bounds" if (row < 0 || row > 7 || col < 0 || col > 7)
+    self.update(row: row, col:col)
+  end
 end
