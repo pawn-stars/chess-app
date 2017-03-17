179f7b518e4e4ba670b5fbf892fb3dfe766a998b
Jacob Gillespie <jacobwgillespie@gmail.com>
2017-03-17 12:34:32 -0500

    Fix rubocop issues

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,14 +2,14 @@ class Piece < ApplicationRecord
   belongs_to :user
   belongs_to :game
   has_many :moves
-  
+
   scope :are_captured, -> { where(is_captured: true) }
   scope :are_not_captured, -> { where(is_captured: false) }
 
   def self.types
     %w(Pawn Rook Knight Bishop Queen King)
   end
-  
+
   def move_to!(row, col)
     raise "Out of bounds" if row < 0 || row > 7 || col < 0 || col > 7
     update(row: row, col: col)
