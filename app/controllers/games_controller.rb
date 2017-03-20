ddb8d8848900d6234bdb8c9026e8dd7cc85b0768
Melissa Reynolds <mkreynolds@cableone.net>
2017-03-20 02:12:10 +0000

    join game redirects to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,7 +24,7 @@ class GamesController < ApplicationController
 
     @game.update_attributes(game_params)
     if @game.valid?
-      redirect_to games_path
+      redirect_to game_path
     else
       render :index, text: "Not Allowed"
     end
