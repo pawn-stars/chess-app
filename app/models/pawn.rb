765e93f7f6a2dea43cb96b5890fa8a12728842af
Brian Hussey <brianedwardhussey@gmail.com>
2017-03-20 01:08:59 +0000

    unicode representation of pieces

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,11 @@
 class Pawn < Piece
+
+  def display_piece
+    if black?
+      return "&#9823;"
+    else
+      return "&#9817;"
+    end
+  end
+
 end
--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,11 @@
 class Queen < Piece
+
+  def display_piece
+    if black?
+      return "&#9819;"
+    else
+      return "&#9813;"
+    end
+  end
+
 end
