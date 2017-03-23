5542d967a76524bc44f474b29f6b6ed713895751
Melissa Reynolds <mkreynolds@cableone.net>
2017-03-23 19:29:22 +0000

    bishop move_legal? & test

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,7 @@
 class Bishop < Piece
+  def move_legal?(to_row, to_col)
+    row_diff = (self.row - to_row).abs
+    col_diff = (self.col - to_col).abs
+    row_diff == col_diff
+  end
 end
---
+++ spec/models/bishop_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Bishop, type: :model do
+  describe '#move_legal?' do
+    let(:bishop){Bishop.create(row: 3, col: 3)}
+
+    it "is legal" do
+      expect(bishop.move_legal?(4,4)).to be true
+    end
+
+    it "is not legal" do
+      expect(bishop.move_legal?(4,5)).to be false
+    end
+  end
+
+end
