452ff959bf7119132b122caef61951bdc64bf921
Melissa Reynolds <mkreynolds@cableone.net>
2017-03-23 20:35:09 +0000

    changes to bishop_spec.rb

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Bishop, type: :model do
   describe '#move_legal?' do
     let(:bishop) { Bishop.create(row: 3, col: 3) }
 
-    it "is legal" do
-      expect(bishop.move_legal?(4, 4)).to be true
+    it "allows legal moves" do
+      expect(bishop.move_legal?(6, 6)).to be true
     end
 
-    it "is not legal" do
+    it "does not allow not legal moves" do
       expect(bishop.move_legal?(4, 5)).to be false
     end
   end
