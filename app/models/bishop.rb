8d2b142ee9f885a0ab9e9f24361c077876074eee
Melissa Reynolds <mkreynolds@cableone.net>
2017-03-24 20:19:18 +0000

    fixed rubocop errors

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,7 +1,7 @@
 class Bishop < Piece
   def move_legal?(to_row, to_col)
-    row_diff = (self.row - to_row).abs
-    col_diff = (self.col - to_col).abs
+    row_diff = (row - to_row).abs
+    col_diff = (col - to_col).abs
     row_diff == col_diff
   end
 end
