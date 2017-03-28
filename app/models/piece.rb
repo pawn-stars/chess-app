99cfe7894d9a07a48b5fc47d08ff0ce34f0f3089
MarciaOrr <orrmar@swcp.com>
2017-03-28 00:13:31 +0000

    Fixed obvious rubocop errors.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,7 +10,7 @@ class Piece < ApplicationRecord
     %w(Pawn Rook Knight Bishop Queen King)
   end
 
-  # Adds an STI type property to the JSON data, rails doesn't do this be default
+  # Adds an STI type property to the JSON data, rails doesn't do this by default
   def serializable_hash(options = nil)
     super.merge("type" => type)
   end
@@ -21,10 +21,9 @@ class Piece < ApplicationRecord
     from_row = row
     from_col = col
     update_attributes(row: to_row, col: to_col)
-    # create_move!(from_row, from_col)
+    create_move!(from_row, from_col)
+    true
   end
-
-
 
   private
 
@@ -34,7 +33,6 @@ class Piece < ApplicationRecord
     return false unless move_legal?(to_row, to_col)
     return false if move_obstructed?(to_row, to_col)
     return false if move_destination_obstructed?(to_row, to_col)
-
     true
   end
 
@@ -66,66 +64,73 @@ class Piece < ApplicationRecord
     end
 
     obstruction_array = []
-    col_direction = 0
-    row_direction = 0
-
     if row == to_row
-      col_direction = to_col > col ? 1 : -1
-      current_col = col + col_direction
-      while (to_col - current_col).abs > 0
-        obstruction_array << [row,current_col]
-        current_col += col_direction
-      end
-      Rails.logger.debug "Horizonal Move. Inspect #{obstruction_array.size} squares"
+      obstruction_array = move_obstructed_horizontal(to_col)
     elsif col == to_col
-      row_direction = to_row > row ? 1 : -1
-      current_row = row + row_direction
-      while (to_row - current_row).abs > 0
-        obstruction_array << [current_row,col]
-        current_row += row_direction
-      end
-      Rails.logger.debug "Vertical Move. Inspect #{obstruction_array.size} squares"
+      obstruction_array = move_obstructed_vertical(to_row)
     elsif (row - to_row).abs == (col - to_col).abs
-      col_direction = to_col > col ? 1 : -1
-      row_direction = to_row > row ? 1 : -1
-      current_col = col + col_direction
-      current_row = row + row_direction
-      while (to_row - current_row).abs > 0 && (to_col - current_col).abs > 0
-        obstruction_array << [current_row,current_col]
-        current_col += col_direction
-        current_row += row_direction
-      end
-      Rails.logger.debug "Diagonal Move. Inspect #{obstruction_array.size} squares"
+      obstruction_array = move_obstructed_diagonal(to_row, to_col)
     end
 
-    Rails.logger.debug ""
-    Rails.logger.debug "Iterate obstruction_array"
-
+    # Rails.logger.debug "Obstruction array contains #{obstruction_array.size} elements"
     return false if obstruction_array.empty?
     obstruction_array.each do |square|
-      if game.piece_at(square[0],square[1])
-        Rails.logger.debug "Obstructed Square at #{square[0]}, #{square[1]}"
-      end
-      return true if game.piece_at(square[0],square[1])
+      return true if game.piece_at(square[0], square[1])
     end
-    Rails.logger.debug "NO OBSTRUCTION"
+
     false
+  end
+
+  def move_obstructed_horizontal(to_col)
+    obstruction_array = []
+    col_direction = to_col > col ? 1 : -1
+    current_col = col + col_direction
+    while (to_col - current_col).abs > 0
+      obstruction_array << [row, current_col]
+      current_col += col_direction
+    end
+    Rails.logger.debug "Horizonal Move. Inspect #{obstruction_array.size} squares"
+    obstruction_array
+  end
+
+  def move_obstructed_vertical(to_row)
+    obstruction_array = []
+    row_direction = to_row > row ? 1 : -1
+    current_row = row + row_direction
+    while (to_row - current_row).abs > 0
+      obstruction_array << [current_row, col]
+      current_row += row_direction
+    end
+    Rails.logger.debug "Vertical Move. Inspect #{obstruction_array.size} squares"
+    obstruction_array
+  end
+
+  def move_obstructed_diagonal(to_row, to_col)
+    obstruction_array = []
+    col_direction = to_col > col ? 1 : -1
+    row_direction = to_row > row ? 1 : -1
+    current_col = col + col_direction
+    current_row = row + row_direction
+    while (to_row - current_row).abs > 0 && (to_col - current_col).abs > 0
+      obstruction_array << [current_row, current_col]
+      current_col += col_direction
+      current_row += row_direction
+    end
+    Rails.logger.debug "Diagonal Move. Inspect #{obstruction_array.size} squares"
+    obstruction_array
   end
 
   def move_destination_obstructed?(to_row, to_col)
     other_piece = game.piece_at(to_row, to_col)
     if other_piece
       side = "White"
-      if other_piece.is_black
-        side = "Black"
-      end
-      Rails.logger.debug "Destination NOT CLEAR. There is a #{other_piece.type} for side #{side} at destination."
+      side = "Black" unless other_piece.is_black
+      Rails.logger.debug "Destination NOT CLEAR. #{side} #{other_piece.type} at destination."
       if other_piece.user == user
-        Rails.logger.debug "DESTINATION CONTAINS SAME SIDE PIECE. ERROR"
+        Rails.logger.debug "   DESTINATION CONTAINS SAME SIDE PIECE. ERROR"
         true
       else
-        Rails.logger.debug "Destination contains opponent piece. capture possible."
-
+        Rails.logger.debug "   Destination contains opponent piece. capture possible."
         # call capture logic
         other_piece.update(is_captured: true)
         false
@@ -135,8 +140,6 @@ class Piece < ApplicationRecord
       false
     end
   end
-
-
 
   # moved capture piece methods to private - after verify that move to new square is valid
   def capture_piece(row, col)
@@ -154,7 +157,6 @@ class Piece < ApplicationRecord
   def captured?
     is_captured == true
   end
-
 
   def create_move!(from_row, from_col)
     last_move_number = game.moves.last ? game.moves.last.move_number : 0
