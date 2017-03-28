92a9194dd5c199d33989fc7ce32ca4ca631b2be0
jdoleiv <jdoleiv@gmail.com>
2017-03-28 22:51:14 +0800

    Refactored rspec tests and shortened rook move

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,8 +1,6 @@
 class Rook < Piece
   def valid_move?(to_row, to_col)
-    # Returns to false if pathis obstructed, as Rook cannot jump its own pieces
-    # return false if piece.path_obstructed?
-    # Vertical move col stays the same # Horizontal move row stays the same
-    return true if (col - to_col).zero? || (row - to_row).zero?
+    return true if col == to_col || row == to_row
+    false
   end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,38 +1,18 @@
 require 'rails_helper'
 RSpec.describe Rook, type: :model do
   describe "#valid_move?" do
-    before(:all) do
-      User.delete_all
-      Game.delete_all
-      Piece.delete_all
-
-      @user1 = User.create(
-        email: 'foobar@foobar.com',
-        screen_name: 'foobar',
-        password: 'foobar',
-        password_confirmation: 'foobar'
-      )
-      @user2 = User.create(
-        email: 'black@foobar.com',
-        screen_name: 'black',
-        password: 'foobar',
-        password_confirmation: 'foobar'
-      )
-
-      @game = @user1.games.create(
-        white_player_id: @user1,
-        black_player_id: @user2
-      )
-    end
+    let(:rook) { Rook.create(row: 0, col: 1) }
 
     it "should allow legal vertical moves" do
-      rook = Rook.create(row: 0, col: 1, game_id: @game.id)
-      expect(rook.valid_move?(1, 1)).to eq(true)
+      expect(rook.valid_move?(1, 1)).to be true
     end
 
     it "should allow legal horizontal moves" do
-      rook = Rook.create(row: 0, col: 1)
-      expect(rook.valid_move?(0, 0)).to eq(true)
+      expect(rook.valid_move?(0, 0)).to be true
+    end
+
+    it "should not allow an illegal move" do
+      expect(rook.valid_move?(2, 2)).to be false
     end
   end
 end
