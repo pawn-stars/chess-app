e61bd7f8e579c04c1d18015c2c7118deb380f1ba
Jacob Gillespie <jacobwgillespie@gmail.com>
2017-04-04 15:53:50 -0500

    Adjust the move_legal? method for the queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,20 +2,6 @@ class Queen < Piece
   def move_legal?(to_row, to_col)
     row_diff = (row - to_row).abs
     col_diff = (col - to_col).abs
-    row_diff <= 1 && col_diff <= 1
-  end
-
-  def valid_move?(row, col)
-    return false unless super(row, col)
-    return false if path_blocked?(row, col)
-    row_diff = row_diff(row)
-    col_diff = col_diff(col)
-    return true if row_diff == col_diff
-    if row_diff > 0 && col_diff.zero?
-      return true
-    end
-    if row_diff.zero? && col_diff > 0
-      return true
-    end
+    row_diff == col_diff || row_diff == 0 || col_diff == 0
   end
 end
