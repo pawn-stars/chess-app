7b0e9bbe061dd241497af3e43ef826dea1a39f5d
Marcia O <orrmar@swcp.com>
2017-04-04 15:43:28 -0600

    Move obstructed (#32)
    * move obstructed and move destination obstructed for working. problems with capture
    
    * Now can capture opposing piece. it will leave board on game refresh
    
    * Fixed obvious rubocop errors.
    
    * added move_destination_same_side?. changed capture. rubocop and rspec ran
    
    * ally destination work in progress
    
    * move_destination_ally and ally_at methods working.
    
    * move_obstructed? simplified
    
    * commit to protect work in this branch
    
    * fixed ally_at, rook.rb move_legal, piece and rook tests
    
    * move_obstructed and tests complete.
    
    * removed logger.debug from files
    
    * minor cleanups within files. nothing of substance
    
    * resolving conflicts
    
    * changes to valid_move?, move_legal?, move_obstructed? when piece is Knight
    
    * Knight::move_legal?. huge error with constants in piece rspec. rubcocop complexity error continues in valid_move?
    :
    
    * fixed constants and before block error in rspec for pieces
    
    * added Knight::move_obstructed? and parent valid_move? passes rubocop
    
    * merged origin/master into move_obstructed. ready to add to master

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Rook, type: :model do
     let(:rook) { Rook.create(row: 0, col: 1) }
 
     it "should allow legal vertical moves" do
-      expect(rook.valid_move?(1, 1)).to be true
+      expect(rook.move_legal?(1, 1)).to be true
     end
 
     it "should allow legal horizontal moves" do
-      expect(rook.valid_move?(0, 0)).to be true
+      expect(rook.move_legal?(0, 0)).to be true
     end
 
     it "should not allow an illegal move" do
-      expect(rook.valid_move?(2, 2)).to be false
+      expect(rook.move_legal?(2, 2)).to be false
     end
   end
 end
