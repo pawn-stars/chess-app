910c0adfc51b30179ce5f6b48e3412cc43a0bb1d
MarciaOrr <orrmar@swcp.com>
2017-04-04 19:28:16 +0000

    saving work in progress

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,7 +1,66 @@
 class King < Piece
+
+  def valid_move?(to_row, to_col)
+    logger.debug "valid_move for #{type}"
+    return false if move_nil?(to_row, to_col)
+    return false if move_out_of_bounds?(to_row, to_col)
+    return false if move_destination_ally?(to_row, to_col)
+    return false unless move_legal?(to_row, to_col)
+    true
+  end
+
   def move_legal?(to_row, to_col)
+    logger.debug "move_legal for #{type}"
     row_diff = (row - to_row).abs
     col_diff = (col - to_col).abs
-    row_diff <= 1 && col_diff <= 1
-  end # method move_legal?
-end
+    return true if row_diff <= 1 && col_diff <= 1
+
+    legal_castle_move(to_row, to_col)
+  end   # end move_legal?
+
+  private
+
+  def legal_castle_move(to_row, to_col)
+    logger.debug "CASTLE"
+    return false unless moves.empty?
+    return false unless row == to_row   # on same row
+    return false until (col - to_col).abs == 2   # moved 2 spaces
+
+    # verify rook in correct location and has not moved
+    logger.debug("Getting the rook")
+    @rook = nil
+    side = ''
+    new_king_col = new_rook_col = -1
+    if col < to_col   # king side castle
+      side = 'king'
+      new_king_col = 6
+      new_rook_col = 5
+      @rook = rook_at(row,7)
+    else
+      side = 'queen'
+      new_king_col = 2
+      new_rook_col = 3
+      @rook = rook_at(row,0)
+    end
+    return false unless @rook
+
+    return false if move_obstructed?(@rook.row,@rook.col)
+    logger.debug "continue with the castle. clear path"
+
+    update_attributes(row: to_row, col: new_king_col)
+    create_move!(to_row, new_king_col)
+
+    @rook.update_attributes(row: to_row, col: new_rook_col)
+    create_move!(to_row, new_rook_col)
+
+    logger.debug "king and rook should have castled"
+    true
+
+  end
+
+  def rook_at(check_row, check_col)
+    rook = game.piece_at(check_row, check_col)
+    rook if rook && rook.moves.empty?
+  end
+
+end   # Class King
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -18,22 +18,39 @@ class Piece < ApplicationRecord
   def move_to!(to_row, to_col)
     return false unless valid_move?(to_row, to_col)
     # en passant capture won't work if the line below executes after updating coords
-    capture_piece(to_row, to_col)
 
-    from_row = row
-    from_col = col
-    update_attributes(row: to_row, col: to_col)
-    create_move!(from_row, from_col)
+    if capture_piece?(to_row, to_col)
+      update_piece(to_row,to_col,'capturing')
+    else
+      update_piece(to_row,to_col,'moved')
+    end
+
+
+    capture_piece(to_row, to_col)
+    update_piece(to_row,to_col,'moved')
+
+    # from_row = row
+    # from_col = col
+    # update_attributes(row: to_row, col: to_col)
+    # update_piece(to_row, to_col)
+    # create_move!(from_row, from_col)
     true
   end
 
-  def capture_piece(row, col)
-    (enemy = enemy_at(row, col)) ? enemy.captured! : false
+  def update_piece(to_row, to_col, state)
+    from_row = row
+    from_col = col
+    update_attributes(row: to_row, col: to_col)
+    create_move!(from_row,from_col,state)
   end
 
-  def captured!
-    update(row: -1, col: -1)
+  def capture_piece?(row, col)
+    (enemy = enemy_at(row, col)) ? enemy.update_piece(-1,-1,'captured') : false
   end
+
+  # def captured!
+  #   update(row: -1, col: -1)
+  # end
 
   def captured?
     row < 0 && col < 0
@@ -42,10 +59,14 @@ class Piece < ApplicationRecord
   private
 
   def valid_move?(to_row, to_col)
+    logger.debug "valid_move for #{type}"
     return false if move_nil?(to_row, to_col)
     return false if move_out_of_bounds?(to_row, to_col)
+    return false if move_destination_ally?(to_row, to_col)
+
+    return true if type == "Knight"
+
     return false unless move_legal?(to_row, to_col)
-    return false if move_destination_obstructed?(to_row, to_col)
     return false if move_obstructed?(to_row, to_col)
     true
   end
@@ -56,6 +77,11 @@ class Piece < ApplicationRecord
 
   def move_out_of_bounds?(to_row, to_col)
     to_row < 0 || to_row > 7 || to_col < 0 || to_col > 7
+  end
+
+  def move_destination_ally?(to_row, to_col)
+    ally = game.piece_at(to_row, to_col)
+    ally && ally.is_black == is_black ? true : false
   end
 
   def move_legal?(to_row, to_col)
@@ -70,21 +96,28 @@ class Piece < ApplicationRecord
     end
   end
 
-  def move_destination_obstructed?(_to_row, _to_col)
+  def move_obstructed?(to_row, to_col)
+    row_direction = to_row <=> row
+    col_direction = to_col <=> col
+
+    current_row = row + row_direction
+    current_col = col + col_direction
+
+    until current_row == to_row && current_col == to_col
+      return true if game.piece_at(current_row, current_col)
+      current_row += row_direction
+      current_col += col_direction
+    end
     false
   end
 
-  def move_obstructed?(_to_row, _to_col)
-    false
-  end
-
-  def create_move!(from_row, from_col)
+  def create_move!(from_row, from_col, state)
     last_move_number = game.moves.last ? game.moves.last.move_number : 0
     moves.create(
       move_number: last_move_number + 1,
       from_position: [from_row, from_col],
       to_position: [row, col],
-      move_type: nil, # to be implemented later
+      move_type: state, # to be implemented later
       game_id: game.id
     )
   end
