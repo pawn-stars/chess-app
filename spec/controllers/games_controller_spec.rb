7d790660af6b71d334c7443146d209bcdd2714a2
Melissa Reynolds <mkreynolds@cableone.net>
2017-04-06 22:56:15 +0000

    add authenticate to games controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  it "should require user to be logged in to start a game" do
+    post :create, id: games
+    expect(response).to redirect_to new_user_session_path
+  end
 end
