a732f858f7379a3e228743a44cd704a257b3b84a
Melissa Reynolds <mkreynolds@cableone.net>
2017-04-07 01:49:17 +0000

    spec changes

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  it "should require user to be logged in to start a game" do
-    post :create, id: games
-    expect(response).to redirect_to new_user_session_path
+  describe "games#create action" do
+    it "should require user to be logged in to start a game" do
+      Game.create(white_player_id: 1)
+      post :create, id: game.id
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 end
