46100fef2071c6419090b36c0d18c9348a433147
Melissa Reynolds <mkreynolds@cableone.net>
2017-04-08 02:54:31 +0000

    tests working

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,9 +3,26 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   describe "games#create action" do
     it "should require user to be logged in to start a game" do
-      Game.create(white_player_id: 1)
-      post :create, id: game.id
+      game = Game.create
+      post :create, params: { id: game.id }
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe "games#update action" do
+    it "should require user to be logged in to join a game" do
+      game = Game.create
+      patch :update, params: { id: game.id }
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe "games#show action" do
+    it "should require user to be logged in to show a game" do
+      game = Game.create
+      get :show, params: { id: game.id }
       expect(response).to redirect_to new_user_session_path
     end
   end
 end
+
