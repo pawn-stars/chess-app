c777fe4261ba651499417cc6cdb9859fd53fe520
Melissa Reynolds <mkreynolds@cableone.net>
2017-04-18 19:25:58 +0000

    profile page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,11 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+
+  end
+
+  def losses(current_user)
+    return true if @game.winner_id.present? && winner_id != current_user.id
   end
 
 end
